3ca9f60fb60c91b7f8b171c66d4fcc3be8d05132
Sanjay Bhangar <sanjay@mapbox.com>
2017-08-10 17:51:27 +0530

    add members as from, to, via properties

--- opl2json.js
+++ opl2json.js
@@ -18,13 +18,24 @@ const obj = lines.reduce((memo, val) => {
   let tags = cols[7];
   tags = tags.slice(1, tags.length);
   const tagGroups = tags.split(',');
-  const tagObj = tagGroups.reduce((tagMemo, tagVal) => {
+  let tagObj = tagGroups.reduce((tagMemo, tagVal) => {
     const split = tagVal.split('=');
     const key = split[0];
     const val = split[1];
     tagMemo[key] = val;
     return tagMemo; 
   }, {});
+  if (cols.length > 8 && cols[8][0] === 'M') {
+    let members = cols[8];
+    console.log('members', members);
+    members = members.slice(1, members.length);
+    const membersSplit = members.split(',');
+    membersSplit.forEach((member) => {
+      const memberSplit = member.split('@');
+      tagObj[memberSplit[1]] = memberSplit[0];
+    });
+
+  }
   memo[id] = tagObj;
   return memo;
 }, {});
