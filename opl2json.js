9e696eb53c87269c675024a051e80d791bc2d4c8
Sanjay Bhangar <sanjay@mapbox.com>
2017-09-11 15:36:21 +0530

    refactor to export module, add test

--- opl2json.js
+++ opl2json.js
@@ -1,42 +1,56 @@
 'use strict';
 
 const fs = require('fs');
-const argv = require('minimist')(process.argv.slice(2));
-const join = require('path').join;
 
-const inputFile = argv._[0];
-const outFile = inputFile.replace('.opl', '.json');
+if (require.main === module) {
+  const argv = require('minimist')(process.argv.slice(2));
+  const inputFile = argv._[0];
+  const outFile = inputFile.replace('.opl', '.json');
+  const txt = fs.readFileSync(inputFile, 'utf-8');
+  const jsonData = opl2json(txt);
+  fs.writeFileSync(outFile, JSON.stringify(jsonData, null, 2));
+}
 
-const txt = fs.readFileSync(inputFile, 'utf-8');
+module.exports = opl2json;
 
-const lines = txt.split('\n');
+/*
+  Function to convert a string in `opl` format and return a JSON object
 
-const obj = lines.reduce((memo, val) => {
-  if (!val) return memo;
-  const cols = val.split(' ');
-  const id = cols[0];
-  let tags = cols[7];
-  tags = tags.slice(1, tags.length);
-  const tagGroups = tags.split(',');
-  let tagObj = tagGroups.reduce((tagMemo, tagVal) => {
-    const split = tagVal.split('=');
-    const key = split[0];
-    const val = split[1];
-    tagMemo[key] = val;
-    return tagMemo;
+  @param oplTxt {String} opl, as string
+  @returns {Object} converted to JSON object
+*/
+function opl2json(oplTxt) {
+  const lines = oplTxt.split('\n');
+  const obj = lines.reduce((memo, val) => {
+    if (!val) return memo;
+    const cols = val.split(' ');
+    const id = cols[0];
+    let tags = cols[7];
+    tags = tags.slice(1, tags.length);
+    const tagGroups = tags.split(',');
+    let tagObj = tagGroups.reduce((tagMemo, tagVal) => {
+      const split = tagVal.split('=');
+      const key = split[0];
+      const val = split[1];
+      tagMemo[key] = val;
+      return tagMemo;
+    }, {});
+    if (cols.length > 8 && cols[8][0] === 'M') {
+      let members = cols[8];
+      members = members.slice(1, members.length);
+      const membersSplit = members.split(',');
+      membersSplit.forEach((member) => {
+        const memberSplit = member.split('@');
+        tagObj[memberSplit[1]] = memberSplit[0];
+      });
+
+    }
+    memo[id] = tagObj;
+    return memo;
   }, {});
-  if (cols.length > 8 && cols[8][0] === 'M') {
-    let members = cols[8];
-    members = members.slice(1, members.length);
-    const membersSplit = members.split(',');
-    membersSplit.forEach((member) => {
-      const memberSplit = member.split('@');
-      tagObj[memberSplit[1]] = memberSplit[0];
-    });
+  return obj;
+}
 
-  }
-  memo[id] = tagObj;
-  return memo;
-}, {});
 
-fs.writeFileSync(outFile, JSON.stringify(obj, null, 2));
+
+
